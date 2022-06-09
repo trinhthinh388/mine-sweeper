360604c923e37f4f8593011ded7c7669cd7a6661
thinh.trinh <thinhtd@wolffungame.com>
2022-06-09 19:52:10 +0700

    feat: updated landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useSpring, animated } from 'react-spring';
-
 // Styles
 import styles from './styles.module.scss';
 
@@ -8,34 +6,26 @@ import styles from './styles.module.scss';
 import { Button } from 'components';
 
 const Landing: React.FC = () => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
   const [showDifficulty, setShow] = React.useState<boolean>(false);
-
-  const animStyles = useSpring({
-    opacity: showDifficulty ? 0 : 1,
-  });
-  const difficultStyles = useSpring({
-    opacity: showDifficulty ? 1 : 0,
-    transform: showDifficulty ? 'scale(1)' : 'scale(0)',
-  });
 
   const onPlayClick = () => setShow(prev => !prev);
 
   return (
-    <div className={styles.container}>
+    <div ref={containerRef} className={styles.container}>
       <h1 className={styles.title}>
         Mine<span>Sweeper</span>
       </h1>
 
       <div className={styles.action}>
-        <animated.div style={animStyles}>
-          <Button onClick={onPlayClick}>Play!</Button>
-        </animated.div>
-
-        <animated.div style={difficultStyles}>
-          <h2 onClick={onPlayClick}>Difficulty</h2>
-          <Button>Easy</Button>
-          <Button>Hard</Button>
-        </animated.div>
+        {!showDifficulty && <Button onClick={onPlayClick}>Play!</Button>}
+        {showDifficulty && (
+          <>
+            <h2>Difficulty</h2>
+            <Button>Easy</Button>
+            <Button>Hard</Button>
+          </>
+        )}
       </div>
     </div>
   );
